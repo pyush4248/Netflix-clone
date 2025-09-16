93ae543acb3eb9969548aaf30e9b3ed014c9afb5
pyush4248 <112476313+pyush4248@users.noreply.github.com>
2025-09-16 20:48:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-import 'react-toastify/dist/ReactToastify.css'
 createRoot(document.getElementById('root')).render(
 
  
